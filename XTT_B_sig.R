29dc1d6b7f916a4ddc160685e7ff4d8f2e046fb8
Herbstlaub89 <thomas.enzlein@gmail.com>
2017-04-24 16:18:11 +0200

    Update XTT_B_sig.R

--- XTT_B_sig.R
+++ XTT_B_sig.R
@@ -5,11 +5,9 @@
 # This script generates a boxplot for plate reader data in the 96-well format  #
 # to compare the fold change between irradiated and control samples.           #
 # A stutent's t-test is done and p-value levels between groups are shown       #
-# with stars over the groups. (* p < 0.05, ** p< 0.01, *** p< 0.001)           #
-# Important: Only graphs with 3 or 4 groups (eg. Control, 24h, 48h and 72h)    #
-# can be processed currently!                                                  #
-# To prepare the data for imput please use Excel_Export_R.xlsx                 #
-# The constants in the next part, control the basics of the plot (like the     #    
+# with stars over the groups (* p < 0.05, ** p< 0.01, *** p< 0.001).           #
+# To prepare the data for imput please use "Excel_Export_R.xlsx".              #
+# The constants in the first part control the basics of the plot (like the     #    
 # titel but there are also options to exclude columns and rows of the plates). #
 # Please change these values to meet your needs.                               #
 ################################################################################
